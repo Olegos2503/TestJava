77b2f48613507d717629437e52e76611a0fca05d
Olegos2503 <112779251+Olegos2503@users.noreply.github.com>
2022-10-05 14:10:39 +0300

    Update and rename src/test/java/BonusServiceTest.java to Src/тест/java/BonusServiceTest.java

--- Src/тест/java/BonusServiceTest.java
+++ Src/тест/java/BonusServiceTest.java
@@ -66,19 +66,5 @@ public class BonusServiceTest {
     }
 
 
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForUnderRegisteredAndOverLimitNegative() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 30_000;
-        boolean registered = false;
-        long expected = 500;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
+  
 }
